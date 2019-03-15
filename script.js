b9397eaf70cf630f6a3510090206e4a9386fabfd
124 <123@macbook-pro-karol-2.home>
2019-03-15 23:37:44 +0100

    meh7

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  // const apiRoot = 'https://desolate-castle-41450.herokuapp.com/v1/task';
+  const apiRoot = 'https://desolate-castle-41450.herokuapp.com/v1/task';
   const trelloApiRoot = 'https://desolate-castle-41450.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
