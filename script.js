4d1d04f58855bc7c83f0b94ce6e53f19615ec7ae
124 <123@macbook-pro-karol-2.home>
2019-03-15 23:17:26 +0100

    v9

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://desolate-castle-41450.herokuapp.com/v1';
+  const trelloApiRoot = 'https://desolate-castle-41450.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
